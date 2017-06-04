617312208dbcf8913071ff7e569edaf4a41c927a
Michael Nau <karl.weber33@gmail.com>
2017-06-04 11:09:56 +0200

    Added LGPL 3.0 license text to all files.

--- wstring.c
+++ wstring.c
@@ -1,3 +1,19 @@
+/*  Copyright (c) 2017 Michael Nau
+    This file is part of Workhorse String.
+
+    Workhorse String is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Workhorse String is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Workhorse Array.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #define _GNU_SOURCE
 #include "wstring.h"
 #include <assert.h>
--- wstring.h
+++ wstring.h
@@ -1,6 +1,22 @@
 /**	@file
 	Dynamically growing C strings with the power and ease of a scripting language.
 */
+/*  Copyright (c) 2017 Michael Nau
+    This file is part of Workhorse String.
+
+    Workhorse String is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Workhorse String is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Workhorse Array.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef WSTRING_H_INCLUDED
 #define WSTRING_H_INCLUDED
 
